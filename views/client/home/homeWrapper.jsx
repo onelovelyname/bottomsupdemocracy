2f5b27173b5446f62670729312fe0e25367cbe5a
krpeacock <kylpeacock@gmail.com>
2016-12-10 16:59:59 -0800

    preparing issuetiles and issuepage

--- views/client/home/homeWrapper.jsx
+++ views/client/home/homeWrapper.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
-
+import $ from 'jquery'
+import IssueTile from './issueTile.jsx'
 
 export default React.createClass({
 
-  componentWillMount: function componentWillMount() {
-    
+  getInitialState : function getInitialState() {
+      return {
+          issues : [],
+      };
   },
 
-  componentDidMount: function(){
-      console.log(this.divRef);
+  componentWillMount: function componentWillMount() {
+    $.getJSON("/api/issues").then((function (issues) {
+      this.setState({
+        issues: issues
+      });
+    }.bind(this)));
+  },
+
+  componentDidMount: function componentDidMount() {
+    console.log(this.state.issues);
   },
 
   divRef: HTMLDivElement,
 
   render(){
+    // if (this.props.issues){
+    //   var issueStates = this.props.issues.map((v,i)=>{
+    //     return (
+    //       <div className="issueTile" key={i}>
+    //         <IssueTile
+    //           {...v}
+    //         />
+        
+    //       </div>
+    //     )
+    //   })
     return <div ref={(elt) => {
        this.divRef = elt;
    }}>
---
+++ views/client/home/issueTile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default React.createClass({
+
+  
+  render(){
+
+    return <div>
+      I'm an issue
+    </div>
+  }
+})
